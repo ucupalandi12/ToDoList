ec131596a22d3fba2ce83f38759f1205d4b88c7c
ucupalandi12 <ucupers1297@gmail.com>
2018-04-04 15:15:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ucupalandi12/todolist/TaskContract.java
@@ -0,0 +1,17 @@
+package com.example.ucupalandi12.todolist;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by ucupalandi12 on 08/03/2018.
+ */
+
+public class TaskContract{
+    public static final String DB_NAME = "com.example.admin.todolist.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
